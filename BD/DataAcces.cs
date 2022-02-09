eb500c48d6769049f97f19836e1e1c7c5cc925ca
ABIMAEL MESEN <amesenc0498@uvirtual.ac.cr>
2022-02-08 19:13:34 -0600

    Clase 1

--- BD/DataAcces.cs
+++ BD/DataAcces.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,37 @@ namespace BD
 {
     public class DataAcces
     {
+        private readonly IConfiguration config;
+
+        public DataAcces(IConfiguration _Config)
+        {
+            config = _Config;
+        }
+
+        public SqlConnection DbConnection => new SqlConnection(
+            new SqlConnectionStringBuilder(config.GetConnectionString("Conn")).ConnectionString);
+
+        //Representacion de retorno de una lista
+        public async Task<IEnumerable<T>> QueryAsync<T>(string sp, object Param = null, int? Timeout = null)
+        {
+            try
+            {
+                using (var exec= DbConnection)
+                {
+                    await exec.OpenAsync();
+
+                    var result = exec.QueryAsync<T>(sql: sp, param: Param, commandType: System.Data.CommandType.StoredProcedure, commandTimeout: Timeout);
+
+                        return await result;
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
     }
 }
